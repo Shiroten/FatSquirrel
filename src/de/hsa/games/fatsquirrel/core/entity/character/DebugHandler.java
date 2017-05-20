8640a14bbee86e1191e8dafddb329983485b22a1
Shiroten <shiroten.xisatu@gmail.com>
2017-05-20 14:01:09 +0200

    Too lazy to write descriptive message

---
+++ src/de/hsa/games/fatsquirrel/core/entity/character/DebugHandler.java
@@ -0,0 +1,31 @@
+import de.hsa.games.fatsquirrel.core.entity.character.MasterSquirrelBot;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+import de.hsa.games.fatsquirrel.core.entity.character.MasterSquirrelBot.ControllerContextImpl;
+
+public class DebugHandler implements InvocationHandler  {
+    private ControllerContextImpl view;
+
+    public DebugHandler(ControllerContextImpl view)  { this.view = view; }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.print("* calling method " + method + " with params ");
+        for (int i = 0; i < args.length; i++)
+            System.out.print(" " + args[i]);
+        System.out.println();
+
+        Object result = null;
+        try  {
+            result = method.invoke(view, args);
+        } catch(IllegalAccessException ex)  {
+            ex.printStackTrace();
+        } catch(InvocationTargetException ex)  {
+            System.out.println("* exception:" + ex.getTargetException());
+            throw ex.getTargetException();
+        }
+        System.out.println("* result:" + result);
+        return result;
+    }
+}
