6214620bcf85353a061d7b7ee44901baa6b1e39c
Shiroten <shiroten.xisatu@gmail.com>
2017-05-28 15:33:02 +0200

    DebugHandler implentation

--- src/de/hsa/games/fatsquirrel/core/entity/character/DebugHandler.java
+++ src/de/hsa/games/fatsquirrel/core/entity/character/DebugHandler.java
@@ -3,29 +3,36 @@ package de.hsa.games.fatsquirrel.core.entity.character;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.InvocationTargetException;
+
 import de.hsa.games.fatsquirrel.core.entity.character.MasterSquirrelBot.ControllerContextImpl;
 
-/*public class DebugHandler implements InvocationHandler  {
+public class DebugHandler implements InvocationHandler {
     private ControllerContextImpl view;
 
-    public DebugHandler(ControllerContextImpl view)  { this.view = view; }
+    public DebugHandler(ControllerContextImpl view) {
+        this.view = view;
+    }
 
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.print("* calling method " + method + " with params ");
-        for (int i = 0; i < args.length; i++)
-            System.out.print(" " + args[i]);
-        System.out.println();
+
+        String output = "* calling method " + method + " with params ";
+        if (args != null) {
+            for (int i = 0; i < args.length; i++)
+                output = output + " " + args[i];
+            output = output + String.format("%n");
+        }
 
         Object result = null;
-        try  {
+        try {
             result = method.invoke(view, args);
-        } catch(IllegalAccessException ex)  {
+        } catch (IllegalAccessException ex) {
             ex.printStackTrace();
-        } catch(InvocationTargetException ex)  {
-            System.out.println("* exception:" + ex.getTargetException());
+        } catch (InvocationTargetException ex) {
+            output = output + "* exception:" + ex.getTargetException();
             throw ex.getTargetException();
         }
-        System.out.println("* result:" + result);
+        output = output + "* result:" + result;
+        //System.out.println(output);
         return result;
     }
-}*/
+}
