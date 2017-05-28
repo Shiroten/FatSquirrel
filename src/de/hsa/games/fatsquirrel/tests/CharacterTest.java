564717879d367a4403b4184b1bbecac2efa1cf7f
Shiroten <shiroten.xisatu@gmail.com>
2017-05-28 16:48:08 +0200

    Push for synchronisation

--- src/de/hsa/games/fatsquirrel/tests/CharacterTest.java
+++ src/de/hsa/games/fatsquirrel/tests/CharacterTest.java
@@ -67,7 +67,6 @@ public class CharacterTest {
             master.nextStep(flat);
             viewMaster = new MasterSquirrelBot.ControllerContextImpl(flat, master);
         }
-
     }
 
     @org.junit.Test
